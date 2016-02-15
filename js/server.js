ea973275f08f7203629e7d4d2253f870b88cf983
bni <bnilsson@me.com>
2016-02-15 22:54:09 +0100

    Cleanup old cruft, use 4 spaces for indent

--- js/server.js
+++ js/server.js
@@ -1,207 +1,207 @@
 orbium = {};
 
-(function(orbium, undefined) {
-	var net = require("net");
-	var crypto = require("crypto");
+(function(orbium) {
+    var net = require("net");
+    var crypto = require("crypto");
 
-	require("./level_show.js");
-	require("./util.js");
-	require("./sprite.js");
-	require("./tile.js");
-	require("./emptytile.js");
-	require("./rotator.js");
-	require("./horiztile.js");
-	require("./verttile.js");
-	require("./crosstile.js");
-	require("./counter.js");
-	require("./announcer.js");
-	require("./timer.js");
-	require("./clock.js");
-	require("./teleporter.js");
-	require("./inspector.js");
-	require("./transformer.js");
-	require("./director.js");
-	require("./matcher.js");
-	require("./sequencer.js");
-	require("./indicator.js");
-	require("./falltile.js");
-	require("./lane.js");
-	require("./bar.js");
-	require("./marble.js");
-	require("./dockee.js");
-	require("./machine.js");
+    require("./level_show.js");
+    require("./util.js");
+    require("./sprite.js");
+    require("./tile.js");
+    require("./emptytile.js");
+    require("./rotator.js");
+    require("./horiztile.js");
+    require("./verttile.js");
+    require("./crosstile.js");
+    require("./counter.js");
+    require("./announcer.js");
+    require("./timer.js");
+    require("./clock.js");
+    require("./teleporter.js");
+    require("./inspector.js");
+    require("./transformer.js");
+    require("./director.js");
+    require("./matcher.js");
+    require("./sequencer.js");
+    require("./indicator.js");
+    require("./falltile.js");
+    require("./lane.js");
+    require("./bar.js");
+    require("./marble.js");
+    require("./dockee.js");
+    require("./machine.js");
 
-	var handshake = function(client, data) {
-		var request = (""+data).split("\r\n");
+    var handshake = function(client, data) {
+        var request = (""+data).split("\r\n");
 
-		var k1 = request[5].split(": ")[1];
-		var k2 = request[6].split(": ")[1];
+        var k1 = request[5].split(": ")[1];
+        var k2 = request[6].split(": ")[1];
 
-		var n1 = parseInt(k1.replace(/[^\d]/g, ''));
-		var n2 = parseInt(k2.replace(/[^\d]/g, ''));
+        var n1 = parseInt(k1.replace(/[^\d]/g, ''));
+        var n2 = parseInt(k2.replace(/[^\d]/g, ''));
 
-		var s1 = k1.replace(/[^ ]/g, '').length;
-		var s2 = k2.replace(/[^ ]/g, '').length;
+        var s1 = k1.replace(/[^ ]/g, '').length;
+        var s2 = k2.replace(/[^ ]/g, '').length;
 
-		n1 /= s1;
-		n2 /= s2;
+        n1 /= s1;
+        n2 /= s2;
 
-		var head = request[8];
+        var head = request[8];
 
-		var md5 = crypto.createHash("md5");
+        var md5 = crypto.createHash("md5");
 
-		md5.update(String.fromCharCode(
-			n1 >> 24 & 0xFF,
-			n1 >> 16 & 0xFF,
-			n1 >> 8 & 0xFF,
-			n1 & 0xFF));
+        md5.update(String.fromCharCode(
+            n1 >> 24 & 0xFF,
+            n1 >> 16 & 0xFF,
+            n1 >> 8 & 0xFF,
+            n1 & 0xFF));
 
-		md5.update(String.fromCharCode(
-			n2 >> 24 & 0xFF,
-			n2 >> 16 & 0xFF,
-			n2 >> 8 & 0xFF,
-			n2 & 0xFF));
+        md5.update(String.fromCharCode(
+            n2 >> 24 & 0xFF,
+            n2 >> 16 & 0xFF,
+            n2 >> 8 & 0xFF,
+            n2 & 0xFF));
 
-		md5.update(head);
+        md5.update(head);
 
-		var response = [
-			"HTTP/1.1 101 WebSocket Protocol Handshake",
-			"Upgrade: WebSocket",
-			"Connection: Upgrade",
-			"Sec-WebSocket-Origin: "+request[4].split(": ")[1],
-			"Sec-WebSocket-Location: ws://"+request[3].split(": ")[1]+"/",
-			"",
-			md5.digest("binary")];
+        var response = [
+            "HTTP/1.1 101 WebSocket Protocol Handshake",
+            "Upgrade: WebSocket",
+            "Connection: Upgrade",
+            "Sec-WebSocket-Origin: "+request[4].split(": ")[1],
+            "Sec-WebSocket-Location: ws://"+request[3].split(": ")[1]+"/",
+            "",
+            md5.digest("binary")];
 
-		client.socket.write(response.join("\r\n"), "binary");
-	};
+        client.socket.write(response.join("\r\n"), "binary");
+    };
 
-	var send = function(client, msg) {
-		client.socket.write("\u0000", "binary");
-		client.socket.write(JSON.stringify(msg), "utf8");
-		client.socket.write("\uffff", "binary");
-	};
+    var send = function(client, msg) {
+        client.socket.write("\u0000", "binary");
+        client.socket.write(JSON.stringify(msg), "utf8");
+        client.socket.write("\uffff", "binary");
+    };
 
-	orbium.Server = function() {
-		var clients = [];
-		
-		this.addClient = function(client) {
-			orbium.Util.addArrayElement(clients, client);			
-		};
+    orbium.Server = function() {
+        var clients = [];
 
-		this.removeClient = function(client) {
-			orbium.Util.removeArrayElement(clients, client);
-		};
+        this.addClient = function(client) {
+            orbium.Util.addArrayElement(clients, client);
+        };
 
-		this.broadcast = function(msg, exclude) {
-			for (var i = 0, j = clients.length; i < j; i++) {
-				var client = clients[i];
+        this.removeClient = function(client) {
+            orbium.Util.removeArrayElement(clients, client);
+        };
 
-				if (exclude === undefined || client !== exclude) {
-					send(client, msg);
-				}
-			}
-		};
-	};
+        this.broadcast = function(msg, exclude) {
+            for (var i = 0, j = clients.length; i < j; i++) {
+                var client = clients[i];
 
-	orbium.server = new orbium.Server();
+                if (exclude === undefined || client !== exclude) {
+                    send(client, msg);
+                }
+            }
+        };
+    };
 
-	orbium.has_dom = false;
-	orbium.has_transform = false;
-	orbium.has_canvas = false;
+    orbium.server = new orbium.Server();
 
-	orbium.has_touch_screen = false;
-	orbium.has_touch_api = false;
+    orbium.has_dom = false;
+    orbium.has_transform = false;
+    orbium.has_canvas = false;
 
-	orbium.Machine.timeLimits = false;
-	orbium.Machine.editorMode = false;
-	orbium.Machine.horizTiles = 8;
-	orbium.Machine.vertTiles = 5;
+    orbium.has_touch_screen = false;
+    orbium.has_touch_api = false;
 
-	orbium.Tile.size = 128;
-	orbium.Marble.size = 40;
-	orbium.Bar.height = 57;
+    orbium.Machine.timeLimits = false;
+    orbium.Machine.editorMode = false;
+    orbium.Machine.horizTiles = 8;
+    orbium.Machine.vertTiles = 5;
 
-	var refMarbleSpeed = 48;
-	var refRotatorSpeed = 60;
-	var refTilesize = 36;
-	orbium.Marble.speed =
-		Math.round(orbium.Tile.size/refTilesize*refMarbleSpeed);
-	orbium.Rotator.speed =
-		Math.round(orbium.Tile.size/refTilesize*refRotatorSpeed);
+    orbium.Tile.size = 128;
+    orbium.Marble.size = 40;
+    orbium.Bar.height = 57;
 
-	orbium.level = orbium.level_show;
+    var refMarbleSpeed = 48;
+    var refRotatorSpeed = 60;
+    var refTilesize = 36;
+    orbium.Marble.speed =
+        Math.round(orbium.Tile.size/refTilesize*refMarbleSpeed);
+    orbium.Rotator.speed =
+        Math.round(orbium.Tile.size/refTilesize*refRotatorSpeed);
 
-	orbium.width = orbium.Tile.size*orbium.Machine.horizTiles;
-	orbium.height = orbium.Tile.size*orbium.Machine.vertTiles+
-		orbium.Bar.height;
+    orbium.level = orbium.level_show;
 
-	orbium.machine = new orbium.Machine();
-	orbium.machine.nextLevel();
+    orbium.width = orbium.Tile.size*orbium.Machine.horizTiles;
+    orbium.height = orbium.Tile.size*orbium.Machine.vertTiles+
+        orbium.Bar.height;
 
-	orbium.machine.loaded = true;
-	orbium.machine.first = false;
+    orbium.machine = new orbium.Machine();
+    orbium.machine.nextLevel();
 
-	setInterval(function() {orbium.machine.run();}, 1000/60);
-	
-	var levelStarted = false;
+    orbium.machine.loaded = true;
+    orbium.machine.first = false;
 
-	var listener = net.createServer();
-	listener.listen(1991);
+    setInterval(function() {orbium.machine.run();}, 1000/60);
 
-	listener.on("connection", function (socket) {
-		socket.setEncoding("binary");
+    var levelStarted = false;
 
-		var handshakeComplete = false;
+    var listener = net.createServer();
+    listener.listen(1991);
 
-		var client = {};
-		client.id = orbium.Util.generateUniqeString();
-		client.socket = socket;
-		orbium.server.addClient(client);
+    listener.on("connection", function (socket) {
+        socket.setEncoding("binary");
 
-		socket.on("data", function(data) {
-			if (!handshakeComplete) {
-				handshake(client, data);
+        var handshakeComplete = false;
 
-				client.socket.setNoDelay(true);
-				client.socket.setEncoding("utf8");
+        var client = {};
+        client.id = orbium.Util.generateUniqeString();
+        client.socket = socket;
+        orbium.server.addClient(client);
 
-				console.log(client.id+" connected");
+        socket.on("data", function(data) {
+            if (!handshakeComplete) {
+                handshake(client, data);
 
-				var state = orbium.machine.getState();
+                client.socket.setNoDelay(true);
+                client.socket.setEncoding("utf8");
 
-				send(client, state);
+                console.log(client.id+" connected");
 
-				handshakeComplete = true;
+                var state = orbium.machine.getState();
 
-				if (!levelStarted) {
-					levelStarted = true;
+                send(client, state);
 
-					orbium.machine.startLevel();
-				}
-			} else {
-				// trim padding
-				var received = data.substring(1, data.length-1);
+                handshakeComplete = true;
 
-				var msg = JSON.parse(received);
+                if (!levelStarted) {
+                    levelStarted = true;
 
-				if (msg.rotate !== undefined) {
-					orbium.machine.rotateRotator(msg.rotate);
+                    orbium.machine.startLevel();
+                }
+            } else {
+                // trim padding
+                var received = data.substring(1, data.length-1);
 
-					var dist = {rotate: msg.rotate};
-					orbium.server.broadcast(dist, client);
-				} else if (msg.launch !== undefined) {
-					orbium.machine.launchRotator(msg.launch, msg.dir);
+                var msg = JSON.parse(received);
 
-					var dist = {launch: msg.launch, dir: msg.dir};
-					orbium.server.broadcast(dist, client);
-				}
-			}
-		});
+                if (msg.rotate !== undefined) {
+                    orbium.machine.rotateRotator(msg.rotate);
 
-		socket.on("close", function(had_error) {
-			orbium.server.removeClient(client);
-			console.log(client.id+" disconnected");
-		});
-	});
+                    var dist = {rotate: msg.rotate};
+                    orbium.server.broadcast(dist, client);
+                } else if (msg.launch !== undefined) {
+                    orbium.machine.launchRotator(msg.launch, msg.dir);
+
+                    var dist = {launch: msg.launch, dir: msg.dir};
+                    orbium.server.broadcast(dist, client);
+                }
+            }
+        });
+
+        socket.on("close", function(had_error) {
+            orbium.server.removeClient(client);
+            console.log(client.id+" disconnected");
+        });
+    });
 })(orbium);
--- js/start.js
+++ js/start.js
@@ -1,11 +1,11 @@
 if (top.location !== self.location) { top.location = self.location; }
 
 if (window.applicationCache) {
-	applicationCache.addEventListener("updateready", function() {
-		window.location.reload();
-	});
+    applicationCache.addEventListener("updateready", function() {
+        window.location.reload();
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-	orbium.init();
+    orbium.init();
 }, false);
